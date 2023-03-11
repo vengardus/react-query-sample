c40d822f4ba96287163bbf3347be898c07a69774
ismytv <ismytv@gmail.com>
2023-03-11 12:38:29 -0500

    codigo sin react query

---
+++ readme.md
@@ -0,0 +1,11 @@
+#Ejemplo de React Query
+##Creado con vite : react + ts
+
+###Paquetes
+$ npm i react-query
+
+###Configuración
+Ver https://react-query-v3.tanstack.com/quick-start
+
+
+
---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useReducer, useState } from 'react'
+import './App.css'
+
+const getNumberRandomFromApi = async():Promise<number> => {
+  const api_url = 'https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new'
+  const res = await fetch(api_url)
+  const numberString = await res.text()
+  
+  return +numberString
+}
+
+const App = () => {
+  const [number, setNumber] = useState<number>()
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [error, setError] = useState<string>()
+  const [key, setReFetch] = useReducer((x) => x+1, 0)
+
+  useEffect(() => {
+    setIsLoading(true)
+    getNumberRandomFromApi().then(num => {
+      setNumber(num)
+      setError(undefined)
+    })
+    .catch (error => setError(error.message))
+  }, [key] )
+
+  useEffect(() => {
+    if (number) setIsLoading(false)
+  }, [number])
+
+  useEffect(() => {
+    if ( error ) setIsLoading(false)
+  }, [error])
+
+  return (
+    <div className="App">
+      { 
+        isLoading 
+        ? (<h2>Cargando...</h2>)
+        : (<h2>Número aleatorio: {number}</h2>)
+      }
+
+      {
+        !isLoading && error && ( <h3>Ocurrió un error: {error}</h3>)
+      }
+
+      <button onClick={setReFetch}>
+        Nḿmero Aleatorio
+      </button>
+
+    </div>
+  )
+}
+
+export default App
